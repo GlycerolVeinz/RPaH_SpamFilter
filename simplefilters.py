4b0fe3579146812453d1377d07a047bf28141a93
GlycerolVeinz <safrankou.matvei@gmail.com>
2022-12-11 21:00:27 +0100

    created parent class BasicFilter for step 4

---
+++ simplefilters.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import os
+from utils import write_classification_to_file
+import abc
+
+class BasicFilter(abc.ABC):
+
+    def train(self, path_to_mails: str):
+        pass
+
+    def universal_test(self, path_to_mails: str, write_class: str):
+        path_to_mails = Path(path_to_mails)
+
+        email_classes = dict()
+
+        # for every file in a directory
+        for a_file in os.listdir(path_to_mails):
+            # if it isn't a special file
+            if (a_file.find('!') == 0):
+                pass
+            # naivly categorize email as "OK"
+            else:
+                email_classes[a_file] = "OK"
+        
+        # write that down to a !prediction.txt file
+        write_classification_to_file(path_to_mails / "!prediction.txt", email_classes)
+
+    @abc.abstractmethod
+    def test(self, path_to_mails):
+        pass
