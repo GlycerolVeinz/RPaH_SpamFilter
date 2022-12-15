7c88b7e53ef0e942e9c76a785b1297b6a9422817
GlycerolVeinz <safrankou.matvei@gmail.com>
2022-12-15 13:41:36 +0100

    created list of spamwords in __init__

--- filter.py
+++ filter.py
@@ -3,11 +3,15 @@ from simplefilters import BasicFilter
 
 class MyFilter(BasicFilter):
 
+    def __init__(self):
+        self.spamwords = ["adult", "prince", "money", "cash", "girl", "women", "mom", "xxx", "lonely", "porn", "treasure"]
+
     def train(self, path_to_mails: str):
         # path to mails need to have !truth.txt
         # need to check if key-words appear in mails (like porn, african prince etc.)
             #* tokens:
-            #* adult, prince, money, cash, girl, women, lonely mom, 
+            #* adult, prince, money, cash, girl, women, mom, xxx, lonely
+            #* tokens should be in __init__ as list/dictionary
         # should create workflow for test()
 
         return super().train(path_to_mails)
