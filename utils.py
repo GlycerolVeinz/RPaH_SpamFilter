fb77a8eed99bb50029631e5fcaa14873f4732934
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-01-04 11:09:26 +0100

    cleared tests

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 
+def read_spamwords():
+
+    with open("./spamwords.txt", "r", encoding="utf-8") as sp_file:
+        sp_words = sp_file.read()
+        sp_words = sp_words.split("\n")
+    return sp_words
+    
 def read_classification_from_file(fpath):
     ndict = dict()
     with open(fpath, 'r', encoding='utf-8') as f:
@@ -8,17 +15,9 @@ def read_classification_from_file(fpath):
 
     return ndict
 
-
 def write_classification_to_file(class_file: str, email_types: dict):
 
     with open(class_file, mode='w', encoding='utf-8') as a_file:
 
         for i in email_types.keys():
             a_file.write(f"{i} {email_types[i]}\n")
-
-def read_spamwords():
-    
-    with open("./spamwords.txt", "r", encoding="utf-8") as sp_file:
-        sp_words = sp_file.read()
-        sp_words = sp_words.split("\n")
-    return sp_words
